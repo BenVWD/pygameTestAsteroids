f5f41f56d6ac687676ce7ac64b77df382599a344
BenVWD <benvass2@gmail.com>
2024-10-04 18:44:30 +0100

    added asteroids, players and collision

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
+import sys
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     #gameplay loop starts here
     pygame.init()
+    #setup gameclock
+    game_clock = pygame.time.Clock()
 
+    #delta time
+    dt = 0
+    #Setup screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Setup groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+    #Setup Player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidField = AsteroidField()
+   
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        for obj in updateable:
+            obj.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collision(player) == True:
+                print("Game Over!")
+                sys.exit()
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+    
+    #limiting framerate
+        dt = game_clock.tick(60)/1000
+    
+    #print(dt)
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
